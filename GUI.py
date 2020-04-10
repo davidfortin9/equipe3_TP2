6da8b3cc35992b02a25612281bd7c9f91490629a
Pierre-Olivier Caron <POCaron>
2020-04-10 10:23:32 -0400

    boutton effacer

--- GUI.py
+++ GUI.py
@@ -1,8 +1,8 @@
 from tkinter import *
 import sqlite3
-from mainTests import *
-from PIL import ImageTk, Image
-
+#from main import dist_matrix1
+#import mainTests
+#from PIL import ImageTk, Image
 
 
 root = Tk()
@@ -24,10 +24,10 @@ root.title('Olympus pick system')
 
 '''Frames'''
 
-frame = LabelFrame(root, padx=100, pady=25)
-frame.pack(padx=20, pady=20)
+frame = LabelFrame(root, padx=30, pady=25)
+frame.pack(padx=10, pady=10)
 
-frame1 = LabelFrame(root, padx=100, pady=50)
+frame1 = LabelFrame(root, padx=100, pady=25)
 frame1.pack(padx=20, pady=20)
 
 '''Labels'''
@@ -35,23 +35,15 @@ frame1.pack(padx=20, pady=20)
 label_e1 = Label(frame, text="Entrer la liste d'items à recueillir")
 label_e1.grid(column=0, row=0)
 
-label_e2 = Label(frame1, text='Voici la tournée à effectuer aujourd\'hui:')
+label_e2 = Label(frame1, text='Voici la tournée à effectuer:')
 label_e2.grid(column=0, row=3)
-
-#label_seq_img = Label(img = seq_img)
-#label_seq_img.grid(column=0, row=10)
-
-#label_e3 = Label(frame1, text='Résultats')
-#label_e3.grid(column=1, row=2)
 
 '''Entry'''
 
 e1 = Entry(frame, width=60)
+e1.insert(0, 'sku1, sku2, ...')
 e1.grid(column=0, row=1)
 #e.get()
-
-#e2 = Entry(frame)
-#e2.grid(column=0, row=4)
 
 
 '''Message box'''
@@ -61,13 +53,6 @@ e1.grid(column=0, row=1)
 
 '''Fonctions'''
 
-#def run():
-    #order_list = Label(frame, text=e1.get())
-
-#def run2():
-    #label_e3 = Label(frame, text=e2.get())
-    #label_e3.grid(column=0, row=6)
-
 '''def run3():
     res_label = Label(frame1, text=sku_pick_inst)
     res_label.grid(column=0, row=9)
@@ -75,27 +60,24 @@ e1.grid(column=0, row=1)
 
     print(sku_pick_inst)'''  
 
-def run3():
+def run():
     res_label = Label(frame1, text=e1.get())
     res_label.grid(column=0, row=9)
     e1.delete(0, END)
 
-     
+    def clear_label(widget):
+        widget['text'] = ''
+    era_button = Button(frame1, text='Effacer', padx=30, command= lambda : clear_label(res_label))
+    era_button.grid(column=0, row=10)    
 
 
 '''Bouttons'''
 
-mybutton = Button(frame, text="Lancer", padx=30, command=run3)
+mybutton = Button(frame, text="Lancer", padx=30, command=run)
 mybutton.grid(column=0,row=2)
-
-#mybutton_e1 = Button(frame, text='Lancer', padx=30, command=run2)
-#mybutton_e1.grid(column=0, row=5)
 
 button_quit = Button(root, text='Quitter', command=root.quit)
 button_quit.pack()
-
-#res_button = Button(frame1, text='Obtenir les résultats', padx=30, command=run3)
-#res_button.grid(column=0, row=8)
 
 
 
