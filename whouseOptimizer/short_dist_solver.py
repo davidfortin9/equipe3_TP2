58959d1f422f2ef16f055af09dea1353bde1620f
Marianne Boucher <marianne.boucher.3@ulaval.ca>
2020-04-30 09:07:38 -0400

    short_dist_solver

--- whouseOptimizer/short_dist_solver.py
+++ whouseOptimizer/short_dist_solver.py
@@ -4,19 +4,21 @@ import whouse_modules.whouse as whouse
 dist_matrix1 = [[0, 20, 30, 40], [20, 0, 30, 5], [30, 30, 0, 10], [40, 5, 10, 0]]
 
 def short_dist_solver(sku_pick, whouse, node_pick, start_node_id):
-    node_pick = pickseq.sku_to_node_pick(sku_pick, whouse.slots(), whouse.start_node_id)
-    dist_matrix = pickseq.create_dist_matrix(node_pick, start_node_id, whouse_graph())
+    node_pick = pickseq.sku_to_node_pick(sku_pick, whouse.slots, whouse.start_node_id)
+    dist_matrix = pickseq.create_dist_matrix(node_pick, start_node_id, whouse_graph)
 
     seq = []
     Z = 0
     #associer le node à la bonne liste dans la distance matrix
     for node in node_pick:
-        for n, dist in enumerate(dist_matrix, 1):
-            if node == n:
-                Z = Z + min(dist_matrix[dist])
+        for index, dist in enumerate(dist_matrix, 1):
+            if node == index:
+                dist_to_add = dist_matrix[index]
+                Z = Z + min(dist_to_add[dist])
                 seq.append(node)
-        print(seq)
-        print(Z)
+        
+        #print(seq)
+        #print(Z)
 
     # Calcule la distance entre le start_node et tous les autres noeuds
     # Ajoute le noeud à la pickseq
