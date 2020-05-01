292787325aedf75d83259368b519cffd373da830
Pierre-Olivier Caron <POCaron>
2020-05-01 15:48:09 -0400

    modif config

--- test_lots_petit_entrepot.py
+++ test_lots_petit_entrepot.py
@@ -13,27 +13,15 @@ for solveur_no in solveur_nos:
         for instance_no in instance_nos:
             
             command_line = ['python',
-                            '__main.py__',
+                            '__main__.py',
                             '-c',
                             'data/petit_entrepot/config_solveur_size' + 
                                 str(size_nos) + '_' + 'inst' + str(instance_no) + '.yaml',
                             '-s' + str(solveur_no),    
-                            '-K' + str(pick_no)
+                            '-K' + str(pick_no),
+                            '-o',
+                            'data/resultats/petit_entrepot/solveur' + str(solveur_no) + 
+                                '_size8_inst1_sol.out'
                             ]
             print(command_line)
             subprocess.run(command_line)
-
-#size_nos = range(4,8)
-#instance_nos = range(1,6)
-#for size_no in size_nos:
-#for instance_no in instance_nos:
-#    command_line = ['python',
-#                    '__main__.py',
-#                    '-c',
-#                    'data/petit_entrepot/config_mip_size8_inst' + str(instance_no) + '.yaml'
-#                    ]
-                    #TODO: Automatiser le numéro de size, le numéro d'instance,
-                    #le numéro de pick et le numéro de solver. Le numéro de pick pourra être
-                    #automatiser dans le paramètre -k et le numéro de solver dans -s.
-#    print(command_line)
-#    subprocess.run(command_line)
